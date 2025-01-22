5c919c7d66213c62c89137adfdaf49cf267a85ce
sul1074 <144888312+sul1074@users.noreply.github.com>
2025-01-22 11:13:31 +0900

    2025.01.21

--- code/[BOJ 1629] 곱셈.cpp
+++ code/[BOJ 1629] 곱셈.cpp
@@ -1,28 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using std::cin;
 using std::cout;
 using std::vector;
 
+long long ModExp(long long, long long, long long);
+
 int main(void) {
 	std::ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(0);
 
-	vector<int> digitCount(10, 0);
-	int n;
-	cin >> n;
-	
-	while (n > 0) {
-		digitCount[n % 10]++;
-		n /= 10;
-	}
+	/*
+	* 3**1 * 3**12
+	*        3**6 * 3**6
+	*
+	* 3**1 mod 5 = 3
+	* 3**2 mod 5 = 4
+	* 3**3 mod 5 = 2
+	* 3**4 mod 5 = 1
+	* --------------
+	* 3**5 mod 5 = 3
+	*
+	* a**b mod c = ((a**b/2) mod c) * ((a**b/2) mod c) mod c
+	*/
 
-	for (int i = digitCount.size() - 1; i >= 0; i--) {
-		for (int j = 0; j < digitCount[i]; j++) {
-			cout << i;
-		}
-	}
+	long long a, b, c;
+	cin >> a >> b >> c;
+
+	cout << ModExp(a, b, c);
 
 	return 0;
+}
+
+long long ModExp(long long a, long long b, long long c) {
+	if (b == 0) return 1;
+
+	long long half = ModExp(a, b / 2, c); 
+
+	half = (half * half) % c;  
+
+	if (b % 2 == 1)  
+		half = (half * a) % c;
+
+	return half;
 }
