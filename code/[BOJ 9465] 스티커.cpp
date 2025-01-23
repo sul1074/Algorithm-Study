ccf939fc80877a89e5700a35559694d4b50e57ff
sul1074 <144888312+sul1074@users.noreply.github.com>
2025-01-23 15:56:35 +0900

    2025.01.23
    동적 프로그래밍 - dp 테이블을 2행으로 둬서, 두 개의 행에서 모두 dp를 돌리고 dp의 연산 방향은 무조건 대각선이 되도록 하는 것이 핵심이었던 문제

---
+++ code/[BOJ 9465] 스티커.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::vector;
+
+int main(void) {
+	std::ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+
+	for (int time = 0; time < t; time++) {
+		int n;
+		cin >> n;
+
+		vector<vector<int>> dp(2, vector<int>(n, 0));
+		vector<vector<int>> sticker(2, vector<int>(n, 0));
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> sticker[i][j];
+			}
+		}
+
+		dp[0][0] = sticker[0][0];
+		dp[1][0] = sticker[1][0];
+
+		if (n > 1) {
+			dp[0][1] = dp[1][0] + sticker[0][1];
+			dp[1][1] = dp[0][0] + sticker[1][1];
+		}
+
+		for (int i = 2; i < n; i++) {
+			dp[0][i] = sticker[0][i] + std::max(dp[1][i - 1], dp[1][i - 2]);
+			dp[1][i] = sticker[1][i] + std::max(dp[0][i - 1], dp[0][i - 2]);
+		}
+
+		cout << std::max(dp[0][n - 1], dp[1][n - 1]) << '\n';
+
+	}
+
+	return 0;
+}
