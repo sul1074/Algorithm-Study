e62618f2e38d412f1883d024d55a54b8e3e67d31
sul1074 <144888312+sul1074@users.noreply.github.com>
2025-01-25 14:48:50 +0900

    2025.01.25
    STL의 우선순위 큐를 사용. 우선순위 할당 방식을 절댓값 힙의 동작 방식에 맞게 구조체로 구현하였고, 이를 우선순위 큐를 초기화 할 때 인자로 넣어주어 우선순위 큐가 절댓값 힙처럼 동작하도록 함.

---
+++ code/[BOJ 11286] 절댓값 힙.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <queue>
+
+using std::cin;
+using std::cout;
+using std::vector;
+using std::priority_queue;
+
+class AbsHeap {
+	// 우선순위 큐에서 절대 값이 가장 작은 값이 우선순위가 가장 크도록 조정
+	struct Compare {
+		bool operator()(int a, int b) {
+			if (std::abs(a) == std::abs(b)) {
+				return a > b;
+			}
+			return std::abs(a) > std::abs(b);
+		}
+	};
+	// Compare를 초기화에 사용하여 우선순위 큐의 container 저장 방식 조정
+	priority_queue<int, std::vector<int>, Compare> numList;
+
+public:
+	AbsHeap() {}
+
+	void push(int value) {
+		numList.push(value);
+	}
+
+	int pop() {
+		if (isEmpty()) return 0;
+		else {
+			int top = numList.top();
+			numList.pop();
+
+			return top;
+		}
+	}
+
+	bool isEmpty() const {
+		return numList.empty();
+	}
+
+	~AbsHeap() {}
+};
+
+int main(void) {
+	std::ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	AbsHeap heap;
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int op;
+		cin >> op;
+
+		if (op == 0) cout << heap.pop() << '\n';
+		else heap.push(op);
+	}
+
+	return 0;
+}
