2f9b0a083f38d517eab789bcc7783ea9353c4203
JongHwan Seol <144888312+sul1074@users.noreply.github.com>
2025-03-07 11:59:25 +0900

    2025.03.07

---
+++ code/2025.03/[프로그래머스] 네트워크.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void dfs(int node, const vector<vector<int>>& com, vector<bool>& visited) {
+    visited[node] = 1;
+    
+    for (int i = 0; i < com[node].size(); ++i) {
+        if (visited[i]) continue;
+        
+        if (com[node][i] == 1) {
+            visited[i] = true;
+            
+            dfs(i, com, visited);
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    vector<bool> visited(n, false);
+    
+    for (int i = 0; i < n; ++i) 
+        computers[i][i] = 0;
+    
+    for (int i = 0; i < n; ++i) {
+        if (visited[i]) continue;
+        
+        dfs(i, computers, visited);
+        ++answer;
+    }
+        
+    return answer;
+}
