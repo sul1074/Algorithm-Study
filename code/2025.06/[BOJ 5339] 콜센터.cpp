9b57f6cfd4bda54bcb55aa5e293d02a9aff4d8bd
JongHwan Seol <144888312+sul1074@users.noreply.github.com>
2025-06-04 23:33:06 +0900

    2025.06.04

---
+++ code/2025.06/[BOJ 5339] 콜센터.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    std::cout << "     /~\\\n";
+    std::cout << "    ( oo|\n";
+    std::cout << "    _\\=/_\n";
+    std::cout << "   /  _  \\\n";
+    std::cout << "  //|/.\\" << "|\\\\\n";
+    std::cout << " ||  \\ /  ||\n";
+    std::cout << "============\n";
+    std::cout << "|          |\n";
+    std::cout << "|          |\n";
+    std::cout << "|          |\n";
+
+    return 0;
+}
