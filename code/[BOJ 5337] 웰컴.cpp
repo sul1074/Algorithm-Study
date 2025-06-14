8fc505ee72aff610b6b9d928ea0e5e706931fa80
JongHwan Seol <144888312+sul1074@users.noreply.github.com>
2025-06-14 22:57:06 +0900

    2025.06.14

---
+++ code/[BOJ 5337] 웰컴.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << ".  .   ." << std::endl;
+    std::cout << "|  | _ | _. _ ._ _  _" << std::endl;
+    std::cout << "|/\\|(/.|(_.(_)[ | )(/." << std::endl;
+    return 0;
+}
